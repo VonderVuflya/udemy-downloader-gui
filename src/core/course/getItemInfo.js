188cc60a3c56fa9675c5b176d5724a96dd797522
Sergey Cherkashin <cherkashin@vuflya.com>
2021-07-21 08:53:13 +0300

    refactored /core/course

--- src/core/course/getItemInfo.js
+++ src/core/course/getItemInfo.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export default function (accessToken, courseid) {
+export default (accessToken, courseid) => {
   return axios.get(
     `https://www.udemy.com/api-2.0/courses/${courseid}/cached-subscriber-curriculum-items?page_size=100000`,
     {
